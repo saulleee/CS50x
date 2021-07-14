9b8614c2ea412fa3db880254ae64c8a5c4ba39e8
saulleee <26151666+saulleee@users.noreply.github.com>
2021-07-14 19:18:20 -0400

    Delete week7/pset7/movies directory

--- week7/pset7/movies/2.sql
+++
@@ -1,3 +0,0 @@
--- In 2.sql, write a SQL query to determine the birth year of Emma Stone.
-
-SELECT birth FROM people WHERE name = "Emma Stone";
--- week7/pset7/movies/8.sql
+++
@@ -1,3 +0,0 @@
--- In 8.sql, write a SQL query to list the names of all people who starred in Toy Story.
-
-SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id = (SELECT id FROM movies WHERE title = "Toy Story"));
